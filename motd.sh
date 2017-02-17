633f73ce976e5b7ac788534aa78f24e077bcfb7c
critaL <victornilsson2456@gmail.com>
2017-02-17 10:05:46 +0100

    Added last updated message

--- motd.sh
+++ motd.sh
@@ -5,6 +5,7 @@ CPUCORES=$(cat /proc/cpuinfo | grep -c processor)
 IP=$(curl -s -4 icanhazip.com) > /dev/null
 cpuTemp0="scale=1; $(cat /sys/class/thermal/thermal_zone0/temp)/1000"
 curl -s wttr.in/falköping?lang=sv > /usr/local/bin/wttr
+motdLastUpdate=$(ls -l /etc/motd | awk {'print $8'})
 
 echo "$(tput sgr0)
 $(tput smul)System Status$(tput rmul)
@@ -19,6 +20,8 @@ $(tput smul)Service Status$(tput rmul)
 - PiVPN             = `ps -ef | grep -v grep | grep openvpn | wc -l | sed -e 's/1/working/g' -e's/0/inactive/g' | sed "s,working,$(tput setaf 2)&$(tput sgr0)," | sed "s,inactive,$(tput setaf 1)&$(tput sgr0),"`
 
  `cat '/usr/local/bin/wttr' | head -n 7 | tail -n 5`
+
+Updated $(tput smul)$motdLastUpdate$(tput rmul) 
 " > /etc/motd
 
 
